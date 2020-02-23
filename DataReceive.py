c11cf44b2770c960915310e8ebebc7a653ae5ab8
Astroglia <Astroglia@github.com>
2020-02-23 16:11:16 -0600

    timestamps and video recording implemented

--- DataReceive.py
+++ DataReceive.py
@@ -7,25 +7,39 @@ import numpy as np
 
 latency =  0.05 # 5 milliseconds request
 
-capture = cv2.VideoCapture('http://10.1.10.158:8000/stream.mjpg', cv2.CAP_DSHOW)
+#capture = cv2.VideoCapture('http://10.1.10.158:8000/stream.mjpg')
+capture = cv2.VideoCapture('http://10.42.0.156:8000/stream.mjpg')
 
 total_found = 0
 
+Framelist = []
+
+t_total = 0
 while(True):
     t_start = time.time()
     ret, frame = capture.read()
+    received_request = requests.get('http://10.42.0.156:8000/index.html')
+    print(received_request.headers)
     if frame is not None:
-        print(frame.shape)
+        #Framelist.append(frame)
+        print(type(received_request.headers['Date']))
         total_found = total_found + 1
-   # print(frame.shape)
-   # total_found = total_found + 1
-    print(total_found)
-    t_end = time.time()
-    print(t_end - t_start) 
+    t_end = time.time() 
+    t_total = t_total + (t_end - t_start)
+
+    if total_found >= 100:
+        print(total_found)
+        print(t_total)
+        total_found = 0
+        t_total = 0
+
+    # print(total_found)
+    # t_end = time.time()
+    # print(t_end - t_start) 
 
    # r = requests.get('http://10.1.10.158:8000/stream.mjpg')
     # print(len(r))
     # print(r)
     # print('--')
-    time.sleep(latency)
+   # time.sleep(latency)
 
