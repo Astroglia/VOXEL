4eaca83da9a43d8fb0ba32687118738499c70d6f
Astroglia <Astroglia@github.com>
2020-02-24 15:17:02 -0600

    started moving to object based video streaming and data saving

--- DataReceive.py
+++ DataReceive.py
@@ -4,6 +4,11 @@ import time
 import cv2
 import urllib 
 import numpy as np
+import queue
+import threading
+import os
+
+import ping, socket
 
 latency =  0.05 # 5 milliseconds request
 
@@ -43,3 +48,71 @@ while(True):
     # print('--')
    # time.sleep(latency)
 
+class VideoStreamStorage:
+    def __init__(self, pi_address, framerate, save_folder):
+        self.pi_address = pi_address
+        self.base_page_address = 'http://' + self.pi_address + '/index.html'
+        self.video_stream_address = 'http://' + self.pi_address + '/stream.mjpg'
+        self.framerate = framerate
+
+        #data saving.
+        self.save_folder = save_folder
+        os.mkdir( self.save_folder )
+        os.chmod( self.save_folder, 0o777 )
+        
+        self.streaming = False
+        self.streaming_data = queue.Queue()
+        # TODO saverate
+    
+    def start_streaming_pipeline(self):
+        stream_thread = threading.Thread(target=self.continuous_stream)
+        save_thread = threading.Thread(target=self.continuous_save)
+
+        stream_thread.start()
+        save_thread.start()
+
+    def continuous_stream(self):
+        self.streaming = True
+        
+        capture_object = cv2.VideoCapture(self.video_stream_address)
+
+        while(self.streaming):
+            ### GET STREAMING DATA & HEADER INFORMATION
+            ret, frame = capture_object.read()
+            received_request = requests.get(self.base_page_address)
+            if frame is not None:
+                self.set_streaming_data( [frame, received_request.headers['tstamp'] ])
+            time.sleep(0.001)
+    
+    def continuous_save(self):
+        while(self.streaming):
+            [ stream_data, timestamp ] = self.get_streaming_data()
+
+            savename = self.save_folder + '/'  + timestamp + '.npy'
+            np.savetxt(savename , stream_data)
+            os.chmod(savename, 0o777) 
+
+            time.sleep(0.001)
+
+
+    def check_connections(self):
+        connections_good = True
+        try:
+            ping.verbose_ping(self.base_page_address, count=1)
+            delay = ping.Ping(self.base_page_address, timeout=2000).do()
+        except socket.error:
+            connections_good = False
+            print("ping error when trying to connect to: ", self.base_page_address)
+        try:
+            ping.verbose_ping(self.video_stream_address, count=1)
+            delay = ping.Ping(self.video_stream_address, timeout=2000).do()
+        except socket.error:
+            connections_good = False
+            print("ping error when trying to connect to: ", self.video_stream_address)
+        return connections_good
+
+    def set_streaming_data(self, data):
+        self.streaming_data.put(data)
+    def get_streaming_data(self):
+        return self.streaming_data.get()
+    
